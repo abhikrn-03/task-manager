598641636d5ab0814fa19cb36f2c82e0efe31f9a
abhikrn-03 <abhik333333@gmail.com>
2020-11-03 07:41:58 +0530

    Auth start

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs')
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -19,12 +20,13 @@ const userSchema = mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
         validate(value) {
-            if (!validator.isEmail(value)){
-                throw new Error('Not a valid email ID')
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
             }
         }
     },
@@ -37,6 +39,32 @@ const userSchema = mongoose.Schema({
             }
         }
     }
+})
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+// Hash the plain text password
+userSchema.pre('save', async function(next) {
+    const user = this
+    if (user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
 })
 
 const User = mongoose.model('User', userSchema)
